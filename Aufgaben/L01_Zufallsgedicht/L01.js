cecdc99d054a459dd88cddf89d5ef48fbae692d4
Anykhet <anykhet@hotmail.com>
2022-10-12 16:05:30 +0200

    skra 2

--- Aufgaben/L01_Zufallsgedicht/L01.js
+++ Aufgaben/L01_Zufallsgedicht/L01.js
@@ -5,7 +5,27 @@ var Zufallsgedicht;
     let s = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let p = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let o = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    console.log(s, p, o);
+    console.log(s);
+    console.log(p);
+    console.log(o);
     //for-schleife
+    for (let index = 6; index >= 1; index--) {
+        let letzterSatz = getVerse(s, p, o);
+        console.log(letzterSatz);
+        //console.log(index)
+        // getVerse(s,p,o);
+        // console.log(getVerse);
+    }
+    //getVerse Funktion
+    function getVerse(_s, _p, _o) {
+        let randoms = Math.floor(Math.random() * _s.length);
+        let randomp = Math.floor(Math.random() * _p.length);
+        let randomo = Math.floor(Math.random() * _o.length);
+        let verse = _s[randoms] + " " + _p[randomp] + " " + _o[randomo] + ".";
+        _s.splice(randoms, 1);
+        _p.splice(randomp, 1);
+        _o.splice(randomo, 1);
+        return verse;
+    }
 })(Zufallsgedicht || (Zufallsgedicht = {}));
 //# sourceMappingURL=L01.js.map
--- Aufgaben/L01_Zufallsgedicht/L01.ts
+++ Aufgaben/L01_Zufallsgedicht/L01.ts
@@ -8,9 +8,36 @@ let s: string[]= ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
 let p: string[]= ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
 let o: string[]= ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers" , "Dementoren"];
 
-console.log(s,p,o);
+console.log(s);
+console.log(p);
+console.log(o);
 
 //for-schleife
+
+for (let index: number=6; index >= 1; index--) {
+    let letzterSatz: string = getVerse(s,p,o);
+    console.log(letzterSatz);
+   //console.log(index)
+   // getVerse(s,p,o);
+   // console.log(getVerse);
+
+}
+
+//getVerse Funktion
+
+function getVerse(_s: string[], _p: string[], _o: string[]): string {
+    let randoms: number = Math.floor(Math.random() * _s.length);
+    let randomp: number = Math.floor(Math.random() * _p.length);
+    let randomo: number = Math.floor(Math.random() * _o.length);
+
+    let verse: string = _s[randoms] + " " + _p[randomp] + " " + _o[randomo] + ".";
+
+    _s.splice(randoms, 1);
+    _p.splice(randomp, 1);
+    _o.splice(randomo, 1);
+
+    return verse; 
+}
 
 
 
